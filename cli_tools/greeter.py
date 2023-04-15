f6dae811ccd820f16df9d6e7a6c6839e81bd6795
derek1945 <130199620+derek1945@users.noreply.github.com>
2023-04-15 23:45:46 +0800

    Update greeter.py

--- cli_tools/greeter.py
+++ cli_tools/greeter.py
@@ -6,7 +6,17 @@ import click
 @click.command()
 @click.argument('first_name')
 @click.argument('last_name')
-def greet(first_name, last_name):
+@click.option('--lang',
+              help='Specify language English (en) or Polish (pl)',
+              default='en',
+              type=click.Choice(['en', 'pl']))
+def greet(first_name, last_name, lang):
     """Displays a greeting to the user."""
-    click.echo(f"Hello {first_name} {last_name}")
+    if lang == 'en':
+        greetings = 'Hello'
+    elif lang == 'pl':
+        greetings = 'Czesc'
+    else:
+        raise click.BadOptionUsage('lang', 'Unsupported language.')
+    click.echo(f"{greetings} {first_name} {last_name}")
     
